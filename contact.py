8c56595c49fbb162fdad9e08bded3a3d9ddaacf1
BarablinV <Viktor.Barablin@moex.com>
2018-02-05 17:51:30 +0300

    Создан тест для нового контакта, добавлены вспомогательный класс и методы.

---
+++ contact.py
@@ -0,0 +1,10 @@
+__author__ = 'viktor'
+
+
+class Contact:
+    def __init__(self, firstname, lastname, home_number, mobile_number):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.home_number = home_number
+        self.mobile_number = mobile_number
+
