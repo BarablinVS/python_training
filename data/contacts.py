8426b713020275c9eef7c675e6c812d04bb0b74e
BarablinV <Viktor.Barablin@moex.com>
2018-03-19 14:09:28 +0300

    Задание №19: Реализовать загрузку тестовых данных из файлов

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+testdata = [
+    Contact(name="name1", header="header1", footer="footer1"),
+    Contact(name="name2", header="header2", footer="footer2")
+]
